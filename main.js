d0a003b1c0d9ad5c01b0e797eeda60a8c98a02b5
Joel Banks <joelanbanks@gmail.com>
2024-05-11 18:10:38 -0400

    css + level 1/2 power + multi

--- main.js
+++ main.js
@@ -17,9 +17,17 @@ const unlock_level_1_mult = document.getElementById('level_1_mult');
 let level_1_mult_price = 20;
 let level_1_mult = 1;
 
+const unlock_level_2_mult = document.getElementById('level_2_mult');
+let level_2_mult_price = 500;
+let level_2_mult = 1;
+
 const unlock_level_1_power = document.getElementById('level_1_power');
 let level_1_power_price = 100;
 let level_1_power = 1;
+
+const unlock_level_2_power = document.getElementById('level_2_power');
+let level_2_power_price = 1000;
+let level_2_power = 1;
 
 // Level 1 being clicked
 document.getElementById('main_1').addEventListener('click', function() {
@@ -30,9 +38,7 @@ document.getElementById('main_1').addEventListener('click', function() {
         main_1_prod++;
         //main_1_add = ((level_1_mult)/main_1_prod);
         main_1_add = Math.pow((1/main_1_prod), (1/level_1_power));
-        console.log(main_2_add)
         main_1_add = main_1_add * main_2_add * level_1_mult;
-        console.log(main_2_add)
         main_1_add = Math.round(main_1_add * 100) / 100;
         document.getElementById('main_1_product').textContent = 'Level 1 is making you ' + main_1_add + ' per second';
         }, 1000); // Update every second
@@ -45,8 +51,8 @@ document.getElementById('main_2').addEventListener('click', function() {
 
     timer_2 = setInterval(function() {
         main_2_prod++;
-        main_2_add = 1+(1/main_2_prod);
-        main_2_add = main_2_add * main_3_add;
+        main_2_add = 1+(Math.pow((1/main_2_prod), (1/level_2_power)));
+        main_2_add = main_2_add * main_3_add * level_2_mult;
         main_2_add = Math.round(main_2_add * 100) / 100;
         document.getElementById('main_2_product').textContent = 'Level 2 is multiplying level 1 by ' + main_2_add;
         }, 1000); // Update every second
@@ -83,11 +89,25 @@ document.getElementById('level_1_mult').addEventListener('click', function() {
     document.getElementById('level_1_mult').textContent = 'Level 1 times 2: Cost ' + level_1_mult_price;
 });
 
+document.getElementById('level_2_mult').addEventListener('click', function() {
+    score -= level_2_mult_price;
+    level_2_mult_price = level_2_mult_price * 5;
+    level_2_mult = level_2_mult * 2;
+    document.getElementById('level_2_mult').textContent = 'Level 2 times 2: Cost ' + level_2_mult_price;
+});
+
 document.getElementById('level_1_power').addEventListener('click', function() {
     score -= level_1_power_price;
     level_1_power_price = level_1_power_price * 10;
     level_1_power += 1;
     document.getElementById('level_1_power').textContent = 'Slower L1 decay: Cost ' + level_1_power_price;
+});
+
+document.getElementById('level_2_power').addEventListener('click', function() {
+    score -= level_2_power_price;
+    level_2_power_price = level_2_power_price * 10;
+    level_2_power += 1;
+    document.getElementById('level_2_power').textContent = 'Slower L2 decay: Cost ' + level_2_power_price;
 });
 
 //Update the scre
@@ -111,6 +131,12 @@ function unlockupgrades() {
         unlock_level_1_mult.disabled = true;
     }
 
+    if (score >= level_2_mult_price) {
+        unlock_level_2_mult.disabled = false;
+    } else {
+        unlock_level_2_mult.disabled = true;
+    }
+
     if (score >= 100) {
         unlock_level_3.disabled = false;
     } else {
@@ -121,6 +147,12 @@ function unlockupgrades() {
         unlock_level_1_power.disabled = false;
     } else {
         unlock_level_1_power.disabled = true;
+    }
+
+    if (score >= level_2_power_price) {
+        unlock_level_2_power.disabled = false;
+    } else {
+        unlock_level_2_power.disabled = true;
     }
 }
 
